9899c97440fa40521732c2d853ef2654a8ae18f8
twisty <twisty@vip.qq.com>
2014-08-19 19:29:28 +0800

    添加条码扫描

--- Client/src/main/java/com/twisty/superclient/view/AddGoodsActivity.java
+++ Client/src/main/java/com/twisty/superclient/view/AddGoodsActivity.java
@@ -2,6 +2,8 @@ package com.twisty.superclient.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,23 +14,32 @@ import com.twisty.superclient.R;
 import com.twisty.superclient.bean.DaoSession;
 import com.twisty.superclient.bean.Detail1Data;
 import com.twisty.superclient.bean.Goods;
+import com.twisty.superclient.bean.GoodsDao;
 import com.twisty.superclient.bean.Store;
 import com.twisty.superclient.bean.StoreDao;
+import com.twisty.superclient.bean.UnitDao;
 import com.twisty.superclient.global.SuperClient;
+import com.twisty.superclient.util.CommonUtil;
 import com.twisty.superclient.view.filter.GoodsFilterActivity;
 import com.twisty.superclient.view.filter.StorePop;
 
+import de.greenrobot.dao.query.QueryBuilder;
+
 public class AddGoodsActivity extends BaseActivity implements View.OnClickListener{
-    private StoreDao storeDao;
     private TextView StoreCode,GoodsName,Spec,Unit;
     private EditText GoodsCode,BarCode,Quantity,OrigTaxPrice,Disc,TaxPrice,UnitPrice,TaxRate,TaxAmt,Amount;
     private Detail1Data detail1Data;
+    private StoreDao storeDao;
+    private UnitDao unitDao;
+    private GoodsDao goodsDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_goods);
         DaoSession session = SuperClient.getDaoSession(this);
         storeDao = session.getStoreDao();
+        unitDao = session.getUnitDao();
+        goodsDao = session.getGoodsDao();
         StoreCode = (TextView) findViewById(R.id.StoreCode);
         GoodsName = (TextView) findViewById(R.id.GoodsName);
         Spec = (TextView) findViewById(R.id.Spec);
@@ -46,6 +57,53 @@ public class AddGoodsActivity extends BaseActivity implements View.OnClickListen
         StoreCode.setOnClickListener(this);
         GoodsName.setOnClickListener(this);
         StoreCode.setText(SuperClient.getDefaultStoreCode());
+        BarCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.toString().contains("\n")){
+                    Quantity.requestFocus();
+                    Quantity.setText("1");
+                }
+            }
+        });
+        BarCode.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    String barCode = BarCode.getText().toString().trim();
+                    if(barCode.length()>0){
+                        QueryBuilder<com.twisty.superclient.bean.Unit> unitQueryBuilder = unitDao.queryBuilder();
+                        com.twisty.superclient.bean.Unit unit = unitQueryBuilder.where(UnitDao.Properties.BarCode.eq(barCode)).unique();
+                        if(unit!=null){
+                            QueryBuilder<Goods> goodsQueryBuilder = goodsDao.queryBuilder();
+                            goodsQueryBuilder.where(GoodsDao.Properties.GoodsID.eq(unit.getGoodsID()));
+                            Goods goods = goodsQueryBuilder.unique();
+                            GoodsCode.setText(goods.getGoodsCode());
+                            GoodsName.setText(goods.getGoodsName());
+                            Spec.setText(goods.getSpecs());
+                            Unit.setText(unit.getUnitName());
+                        }else{
+                            GoodsCode.setText("");
+                            GoodsName.setText("");
+                            Spec.setText("");
+                            Unit.setText("");
+                            CommonUtil.showToastError(AddGoodsActivity.this,"没有找到当前条码的货品!");
+                        }
+
+                    }
+                }
+            }
+        });
     }
 
 
